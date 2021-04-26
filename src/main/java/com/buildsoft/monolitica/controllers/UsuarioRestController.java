45fd4ede85e52d59c688e228b876e9e367bd92fa
Developsteven <steven.perez099@gmail.com>
2021-04-26 13:34:25 -0500

    Creando metodo actualizar usuario.

--- src/main/java/com/buildsoft/monolitica/controllers/UsuarioRestController.java
+++ src/main/java/com/buildsoft/monolitica/controllers/UsuarioRestController.java
@@ -13,6 +13,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -94,6 +95,59 @@ public class UsuarioRestController {
 		}
 		return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
 	}
+	
+	@PutMapping("/usuario/{id}")
+	public ResponseEntity<?> update(@RequestBody Usuario usuario, BindingResult result, @PathVariable Long id){
+		
+		Usuario usuarioActual = usuarioServices.findById(id);
+		
+		Usuario usuarioUpdated = null;
+		
+		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
+		if (usuarioActual == null) {
+			response.put("mensaje", "Error: no se pudo editar, el cliente ID: "
+					.concat(id.toString().concat(" no existe en la base de datos!")));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.NOT_FOUND);
+		}
+		
+		try {
+
+			usuarioActual.setApellido(usuario.getApellido());
+			usuarioActual.setNombre(usuario.getNombre());
+			usuarioActual.setMail(usuario.getMail());
+			usuarioActual.setDocumento(usuario.getDocumento());
+			usuarioActual.setTelefono(usuario.getTelefono());
+			usuarioActual.setEstado(usuario.isEstado());
+			usuarioActual.setRol(usuario.getRol());
+			usuarioActual.setTipoDocumento(usuario.getTipoDocumento());
+
+			usuarioUpdated = usuarioServices.create(usuarioActual);
+
+		}catch (DataAccessException e) {
+			response.put("mensaje", "Error al actualizar el cliente en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente ha sido actualizado con éxito!");
+		response.put("cliente", usuarioUpdated);
+
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
+	}
+	
+	
 	
 	@GetMapping("/rol")
 	public List<Rol> listarRoles() {
