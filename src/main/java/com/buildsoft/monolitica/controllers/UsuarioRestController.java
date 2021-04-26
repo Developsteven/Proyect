fd00b8dc3be58fc88825700a563a6d9c40b37631
Developsteven <steven.perez099@gmail.com>
2021-04-26 12:47:51 -0500

    Mejorando el metodo crear usuario para evitar errores.

--- src/main/java/com/buildsoft/monolitica/controllers/UsuarioRestController.java
+++ src/main/java/com/buildsoft/monolitica/controllers/UsuarioRestController.java
@@ -3,11 +3,13 @@ package com.buildsoft.monolitica.controllers;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -38,8 +40,35 @@ public class UsuarioRestController {
 	}
 
 	@PostMapping("/usuario")
-	public Usuario create(@RequestBody Usuario usuario) {
-		return usuarioServices.create(usuario);
+	public ResponseEntity<?> create(@RequestBody Usuario usuario, BindingResult result) {
+		
+		Usuario usuariotNew = null;
+		Map<String, Object> response = new HashMap<>();
+		
+		if(result.hasErrors()) {
+
+			List<String> errors = result.getFieldErrors()
+					.stream()
+					.map(err -> "El campo '" + err.getField() +"' "+ err.getDefaultMessage())
+					.collect(Collectors.toList());
+			
+			response.put("errors", errors);
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.BAD_REQUEST);
+		}
+		
+		try {
+			
+			usuariotNew = usuarioServices.create(usuario);
+			
+		} catch(DataAccessException e) {
+			response.put("mensaje", "Error al realizar el insert en la base de datos");
+			response.put("error", e.getMessage().concat(": ").concat(e.getMostSpecificCause().getMessage()));
+			return new ResponseEntity<Map<String, Object>>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		
+		response.put("mensaje", "El cliente ha sido creado con éxito!");
+		response.put("cliente", usuariotNew);
+		return new ResponseEntity<Map<String, Object>>(response, HttpStatus.CREATED);
 	}
 	
 	
